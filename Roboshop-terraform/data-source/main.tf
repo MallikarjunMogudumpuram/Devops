9789bc839c642fc3eb9e09fb0ff5d75b7e029a4e
Mallikarjun <mallikarjunm303@gmail.com>
2024-12-24 10:52:22 +0530

    update

--- Roboshop-terraform/data-source/main.tf
+++ Roboshop-terraform/data-source/main.tf
@@ -1,7 +1,22 @@
+terraform {
+   required_providers {
+      azurerm = {
+         source  = "hashicorp/azurerm"
+         version = "3.108.0"
+      }
+   }
+}
 data "azurerm_resource_group" "example" {
    name = "project-setup"
 }
 
 output "rg" {
    value = data.azurerm_resource_group.example
+}
+
+#configure the microsoft azure provider
+
+provider "azurerm" {
+   features {}
+   subscription_id = "c1585f6e-0e58-456f-aed6-fc7b78c32d7c"
 }
