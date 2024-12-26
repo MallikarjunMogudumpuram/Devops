9d29f9b21e505cd9ebf0d945308c89a26706931a
Mallikarjun <mallikarjunm303@gmail.com>
2024-12-26 23:37:07 +0530

    commit

--- learn-terraform/Robo-shopv1/vm/main.tf
+++ learn-terraform/Robo-shopv1/vm/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_machine" "main" {
    name                = "${var.component}-nic"
    location            = data.azurerm_resource_group.main.location
    resource_group_name = data.azurerm_resource_group.main.name
-   network_interface_ids = [azurerm_network_interface.main.id]
+   network_interface_ids = azurerm_network_interface[count.index]
    vm_size             = "Standard_DS1_v2"
 
 # Uncomment this line to delete the OS disk automatically when deleting the VM
