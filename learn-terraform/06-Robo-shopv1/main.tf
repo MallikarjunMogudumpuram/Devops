e4159d140db327c47abcaad3ec867a50d0de69ac
Mallikarjun <mallikarjunm303@gmail.com>
2024-12-26 17:24:00 +0530

    commit

--- learn-terraform/06-Robo-shopv1/main.tf
+++ learn-terraform/06-Robo-shopv1/main.tf
@@ -22,7 +22,6 @@ resource "azurerm_virtual_machine" "main" {
    resource_group_name = data.azurerm_resource_group.example.name
    network_interface_ids = [azurerm_network_interface.front-end.id]
    vm_size             = "Standard_DS1_v2"
-}
 
 # Uncomment this line to delete the OS disk automatically when deleting the VM
    delete_os_disk_on_termination = true
@@ -30,29 +29,33 @@ resource "azurerm_virtual_machine" "main" {
 # Uncomment this line to delete the data disks automatically when deleting the VM
 # delete_data_disks_on_termination = true
 
-storage_image_reference {
-publisher = "Canonical"
-offer     = "0001-com-ubuntu-server-jammy"
-sku       = "22_04-lts"
-version   = "latest"
+ storage_image_reference {
+   publisher = "Canonical"
+   offer     = "0001-com-ubuntu-server-jammy"
+   sku       = "22_04-lts"
+   version   = "latest"
 }
 storage_os_disk {
-name              = "myosdisk1"
-caching           = "ReadWrite"
-create_option     = "FromImage"
-managed_disk_type = "Standard_LRS"
+   name              = "myosdisk1"
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
 }
-os_profile {
-computer_name  = "hostname"
-admin_username = "testadmin"
-admin_password = "Password1234!"
+
+ os_profile {
+   computer_name  = "hostname"
+   admin_username = "testadmin"
+   admin_password = "Password1234!"
 }
-os_profile_linux_config {
-disable_password_authentication = false
+
+ os_profile_linux_config {
+   disable_password_authentication = false
 }
+
 tags = {
-environment = "staging"
+   environment = "staging"
 }
+
 }
 
 
@@ -86,6 +89,18 @@ environment = "staging"
 
 
 
+
+
+# output "rg" {
+#    value = data.azurerm_resource_group.example
+# }
+#
+# #configure the microsoft azure provider
+#
+# provider "azurerm" {
+#    features {}
+#    subscription_id = "c1585f6e-0e58-456f-aed6-fc7b78c32d7c"
+# }
 
 
 
