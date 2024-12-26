b443e513e435ec8b968eb3ceb7aa006ed4e98080
Mallikarjun <mallikarjunm303@gmail.com>
2024-12-26 23:36:13 +0530

    commit

--- learn-terraform/Robo-shopv1/vm/main.tf
+++ learn-terraform/Robo-shopv1/vm/main.tf
@@ -10,7 +10,7 @@ allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "main" {
-  count             = length(var.component)
+  count               = length(var.component)
   name                = "${var.component}-nic"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
@@ -18,7 +18,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "internal"
     subnet_id                     = data.azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = [azurerm_public_ip.practise.id]
+    public_ip_address_id          = azurerm_public_ip.practise[count.index]
   }
 }
 
