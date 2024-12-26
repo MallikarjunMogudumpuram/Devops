eb3dd559f61078dabd159990ce7f9fb4a6961834
Mallikarjun <mallikarjunm303@gmail.com>
2024-12-26 19:39:31 +0530

    commit

--- learn-terraform/06-Robo-shopv1/vm/main.tf
+++ learn-terraform/06-Robo-shopv1/vm/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_virtual_machine" "main" {
    version   = "latest"
 }
 storage_os_disk {
-   name              = "myosdisk1"
+   name              = "sample"
    caching           = "ReadWrite"
    create_option     = "FromImage"
    managed_disk_type = "Standard_LRS"
