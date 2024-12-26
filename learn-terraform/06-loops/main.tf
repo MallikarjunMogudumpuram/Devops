e270af95c4c70f1a66610fc77fcb384fe8907d45
Mallikarjun <mallikarjunm303@gmail.com>
2024-12-26 22:53:07 +0530

    commit

--- learn-terraform/06-loops/main.tf
+++ learn-terraform/06-loops/main.tf
@@ -1,4 +1,15 @@
-resource "null_resource" "demo" {
-  count = 10
+# resource "null_resource" "demo" {
+#   count = 10
+#
+# }
 
+
+resource "null_resource" demo {
+  count = length(var.practise)
+}
+
+
+variable "practise" {
+  default = [
+  "apple", "orange", "banana"]
 }
